8b2b16eefc0e408c3a39d5f129d253e638183416
Gui <iroiro.dbm@gmail.com>
2026-01-21 00:18:41 +0530

    Update app.js

--- web/app.js
+++ web/app.js
@@ -1,1 +1,8 @@
+import { translate } from "../engine/translate.js";
+
+document.getElementById("translate").addEventListener("click", () => {
+  const input = document.getElementById("input").value;
+  const output = translate(input);
+  document.getElementById("output").textContent = output;
+});
 
