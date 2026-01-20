c6d69cfb49beda8c3b2ede6bc403ed9a99948e7d
Gui <iroiro.dbm@gmail.com>
2026-01-21 00:17:04 +0530

    Update translate.js

--- engine/translate.js
+++ engine/translate.js
@@ -1,1 +1,17 @@
+import { normalize } from "./normalize.js";
+import { simplify } from "./simplify.js";
+import { reorder } from "./reorder.js";
+import { dictionary } from "./dictionary.js";
+
+export function translate(text) {
+  const normalized = normalize(text);
+  const simplified = simplify(normalized);
+  const words = simplified.split(/\s+/);
+
+  const gloss = reorder(words)
+    .map(word => dictionary[word])
+    .filter(Boolean);
+
+  return gloss.join(" ");
+}
 
