d00168b4d58f6f73af151bd3a74bb83b7e63688d
Gui <iroiro.dbm@gmail.com>
2026-01-21 00:16:30 +0530

    Update dictionary.js

--- engine/dictionary.js
+++ engine/dictionary.js
@@ -1,1 +1,12 @@
+export const dictionary = {
+  welcome: "WELCOME",
+  our: "OUR",
+  ministry: "MINISTRY",
+  deaf: "DEAF",
+  bible: "BIBLE",
+  learn: "LEARN",
+  contact: "CONTACT",
+  about: null,
+  and: null
+};
 
